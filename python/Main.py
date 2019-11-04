dd78c68891e60058a1a86a71ae0a8a3cb53a9071
Paul <45698887+PaulTony7@users.noreply.github.com>
2019-11-04 23:49:50 +0100

    pull request ania

--- python/Main.py
+++ python/Main.py
@@ -86,6 +86,65 @@ paper = Table(
     Column('abstrakt', String)
 )
 
+participant = Table (
+    'participant', meta,
+	Column ('welcomepack_id', Integer),
+	Column ('ticket_id', Integer),
+	Column ('user_id', Integer, primary_key=True)
+)
+
+welcomepack = Table (
+    'welcomepack', meta, 
+	Column('welcomepack_id', Integer, primary_key=True),
+	Column('shirt_size', String)
+)
+
+ticket = Table (
+    'ticket', meta, 
+	Column ('ticket_id', Integer, primary_key=True),
+	Column ('cost', float),
+	Column( 'purchase_date', Date)
+)
+
+grade = Table (
+    'grade', meta,
+	Column('paper_id', Integer, primary_key=True),
+	Column('user_id',  Integer, primary_key=True),
+	Column('grade', float),
+	Column('reason', String)
+)
+
+reviewer = Table (
+    'reviewer', meta,
+	Column('user_id', Integer, primary_key=True).
+	Column('academic_title', String)
+)
+
+administrator = Table (
+    'administrator', meta, 
+	Column('user_id', Integer, primary_key=True),
+	Column('duty', String)
+)
+
+supervision = Table (
+    'supervision', meta,
+	Column('user_id', Integer, primary_key=True),
+	Column('timetable_id', Integer, primary_key=True)
+)
+
+domain_reviewer = Table (
+    'domain_reviewer', meta,
+	Column ('user_id', Integer,primary_key=True),
+	Column('domain_id', Integer, primary_key=True)
+)
+
+participation = Table(
+    'participation', meta,
+	Column('user_id', Integer, primary_key=True),
+	Column('lecture_id', Integer, primary_key=True)
+)
+
+
 conn = engine.connect()
 
 # Create university
