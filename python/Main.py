c12c989a89699f2dd7d352ca5189633fbedc0c53
Burdzi0 <burdzi0@programmer.net>
2019-11-09 20:48:58 +0100

    Almost done - some troubles with last methods, JOIN fails and gives 0 output

--- python/Main.py
+++ python/Main.py
@@ -1,10 +1,11 @@
-from datetime import date
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Time, Float
-from faker import Faker
 import random
-from random import randrange, randint, shuffle, seed
+from datetime import date
+from random import randrange, randint
+
+from faker import Faker
 from faker.providers.job import Provider
+from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Time, Float
+from sqlalchemy import create_engine
 
 meta = MetaData()
 
@@ -148,22 +149,29 @@ fake = Faker()
 
 def generate_domains():
     domains = list(set(Provider.jobs))
-    seed(2020)
-    shuffle(domains)
 
     for dom in range(20):
-        insert_domain = domain.insert().values(domain_name=domains[dom])
+        print_status("domain", dom)
+        insert_domain = domain.insert().values(domain_name=domains[dom][:50])
         conn.execute(insert_domain)
 
 
 def generate_universities():
     for uni in range(40):
-        insert_university = university.insert().values(uni_name=fake.company(),
-                                                       postal_code=fake.postalcode(),
-                                                       town=fake.city(),
-                                                       address=fake.address(),
-                                                       country=fake.country())
-        conn.execute(insert_university)
+        print_status("university", uni)
+        try_insert_without_parameters(create_university_query)
+
+
+def print_status(name, number):
+    print("Generating {} {}".format(name, number), end="\r")
+
+
+def create_university_query():
+    return university.insert().values(uni_name=fake.company(),
+                                      postal_code=fake.postalcode(),
+                                      town=fake.city(),
+                                      address=fake.address()[:100],
+                                      country=fake.country()[:50])
 
 
 def generate_users():
@@ -173,51 +181,78 @@ def generate_users():
     # Select all universities
     university_ids = conn.execute(query).fetchall()
 
-    for i in range(2000):
-        insert_user = user.insert().values(university_id=university_ids[random_from_range(university_ids)][0],
-                                           name=fake.first_name(),
-                                           surname=fake.last_name(),
-                                           email=fake.first_name() + fake.last_name() + '@' + fake.domain_name() + str(random.randrange(0, 200)),
-                                           registration_date=date.today())
-        conn.execute(insert_user)
+    for i in range(3000):
+        print_status("user", i)
+        try_insert(create_users_query, university_ids)
+
+
+def create_users_query(*args):
+    university_ids = args[0]
+    insert_user = user.insert().values(university_id=university_ids[random_from_range(university_ids)][0],
+                                       name=fake.first_name(),
+                                       surname=fake.last_name(),
+                                       email=fake.first_name() + fake.last_name() + '@' + fake.domain_name() + str(
+                                           random.randrange(0, 200)),
+                                       registration_date=date.today())
+    return insert_user
 
 
 def generate_events():
     for i in range(5):
-        insert_event = event.insert().values(event_name=fake.name(),
-                                             organizing_body=fake.company(),
-                                             address=fake.address(),
-                                             postal_code=fake.postalcode(),
-                                             town=fake.city())
-        conn.execute(insert_event)
+        print_status("event", i)
+        try_insert_without_parameters(create_events_query)
+
+
+def create_events_query():
+    insert_event = event.insert().values(event_name=fake.name(),
+                                         organizing_body=fake.company(),
+                                         address=fake.address(),
+                                         postal_code=fake.postalcode(),
+                                         town=fake.city())
+    return insert_event
 
 
 def generate_tickets():
     for i in range(1500):
-        insert_ticket = ticket.insert().values(
-            cost=(random.random() * 200),
-            purchase_date=date.today())
-        conn.execute(insert_ticket)
+        print_status("ticket", i)
+        try_insert_without_parameters(create_ticket_query)
+
+
+def create_ticket_query():
+    insert_ticket = ticket.insert().values(
+        cost=(random.random() * 200),
+        purchase_date=date.today())
+    return insert_ticket
 
 
 def generate_welcomepacks():
-    query = 'SELECT COUNT(*) from user'
-
-    number_of_welcome_packs = conn.execute(query).fetchone()
+    number_of_welcome_packs = 5000
 
     shirt_size = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
 
-    for number in range(number_of_welcome_packs[0]):
-        insert_welcomepack = welcomepack.insert().values(shirt_size=shirt_size[random_from_range(shirt_size)])
-        conn.execute(insert_welcomepack)
+    for number in range(number_of_welcome_packs):
+        print_status("welcomepack", number)
+        try_insert(create_welcomepack_query, shirt_size)
+
+    print(conn.execute('SELECT COUNT(*) from welcomepack').fetchone())
+
+
+def create_welcomepack_query(shirt_size):
+    shirt = shirt_size[random_from_range(shirt_size)]
+    return welcomepack.insert().values(shirt_size=shirt)
 
 
 def generate_classrooms():
     for clas in range(30):
-        insert_class = classroom.insert().values(classroom_number=randint(1, 700),
-                                                 postal_code=fake.postalcode(),
-                                                 town=fake.city(), address=fake.address())
-        conn.execute(insert_class)
+        print_status("classroom", clas)
+        try_insert_without_parameters(create_classroom_query)
+
+
+def create_classroom_query():
+    insert_class = classroom.insert().values(classroom_number=randint(1, 700),
+                                             postal_code=fake.postalcode(),
+                                             town=fake.city(), address=fake.address())
+    return insert_class
 
 
 def generate_timetables():
@@ -227,10 +262,15 @@ def generate_timetables():
     # Select all universities
     event_ids = conn.execute(query).fetchall()
     for tt in range(6):
-        insert_timetable = timetable.insert().values(event_id=event_ids[random_from_range(event_ids)][0],
-                                                     timetable_name=fake.postalcode(),
-                                                     date=fake.date_between(start_date="today", end_date='+30d'))
-        conn.execute(insert_timetable)
+        print_status("timetable", tt)
+        try_insert(create_timetable_query, event_ids)
+
+
+def create_timetable_query(event_ids):
+    insert_timetable = timetable.insert().values(event_id=event_ids[random_from_range(event_ids)][0],
+                                                 timetable_name=fake.postalcode(),
+                                                 date=fake.date_between(start_date="today", end_date='+30d'))
+    return insert_timetable
 
 
 def generate_papers():
@@ -241,56 +281,71 @@ def generate_papers():
     domain_ids = conn.execute(query_domain).fetchall()
 
     for i in range(200):
-        insert_paper = paper.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
-                                             domain_id=domain_ids[random_from_range(domain_ids)][0],
-                                             topic=fake.word(ext_word_list=None),
-                                             abstrakt=fake.fake.paragraphs(nb=3, ext_word_list=None))
-        conn.execute(insert_paper)
+        print_status("paper", i)
+        try_insert(create_paper_query, domain_ids, user_ids)
+
+
+def create_paper_query(domain_ids, user_ids):
+    insert_paper = paper.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
+                                         domain_id=domain_ids[random_from_range(domain_ids)][0],
+                                         topic=fake.word(ext_word_list=None),
+                                         abstrakt=fake.paragraphs(nb=3, ext_word_list=None))
+    return insert_paper
 
 
 def generate_administrators():
-    query = 'SELECT user_id from user'
+    query = 'SELECT user_id from "user"'
 
     user_ids = conn.execute(query).fetchall()
 
     for adm in range(50):
-        insert_administrator = administrator.insert().values(user_id=user_ids[random_from_range(user_ids)],
-                                                             duty=fake.job())
-        conn.execute(insert_administrator)
+        print_status("administrator", adm)
+        try_insert(create_administrators_query, user_ids)
+
+
+def create_administrators_query(user_ids):
+    insert_administrator = administrator.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
+                                                         duty=fake.job() + str(randrange(500)))
+    return insert_administrator
 
 
 def generate_reviewers():
-    # Select query
-    query = 'SELECT user_id from reviewers'
+    query = 'SELECT user_id from "user"'
 
-    # Select all universities
     user_ids = conn.execute(query).fetchall()
+
     for r in range(50):
-        insert_reviewers = timetable.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
-                                                     academic_title=fake.job)
-        conn.execute(insert_reviewers)
+        print_status("reviewer", r)
+        try_insert(create_reviewers_query, user_ids)
+
+
+def create_reviewers_query(user_ids):
+    insert_reviewers = reviewer.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
+                                                academic_title=fake.job() + str(randrange(500)))
+    return insert_reviewers
 
 
 def generate_participants():
     welcomepack_query = 'SELECT welcomepack_id from welcomepack'
     welcomepack_ids = conn.execute(welcomepack_query).fetchall()
-    shuffle(welcomepack_ids)
 
     ticket_query = 'SELECT ticket_id from ticket'
     ticket_ids = conn.execute(ticket_query).fetchall()
-    shuffle(ticket_ids)
 
-    user_query = 'SELECT user_id from user'
+    user_query = 'SELECT user_id from "user"'
     user_ids = conn.execute(user_query).fetchall()
-    shuffle(user_ids)
 
     for part in range(1500):
-        insert_participant = participant.insert().values(
-            welcomepack_id=welcomepack_ids[part],
-            ticket_id=ticket_ids[part],
-            user_id=user_ids[part])
+        print_status("participant", part)
+        try_insert(create_participant_query, ticket_ids, user_ids, welcomepack_ids)
 
-        conn.execute(insert_participant)
+
+def create_participant_query(ticket_ids, user_ids, welcomepack_ids):
+    insert_participant = participant.insert().values(
+        welcomepack_id=welcomepack_ids[random_from_range(welcomepack_ids)][0],
+        ticket_id=ticket_ids[random_from_range(ticket_ids)][0],
+        user_id=user_ids[random_from_range(user_ids)][0])
+    return insert_participant
 
 
 def generate_suprevisions():
@@ -299,20 +354,31 @@ def generate_suprevisions():
 
     user_ids = conn.execute(query2).fetchall()
     timetable_ids = conn.execute(query1).fetchall()
+
     for i in range(len(timetable_ids)):
-        insert_supervision = supervision.insert().values(user_id=user_ids[random_from_range(user_ids)],
-                                                         timetable_id=timetable_ids[random_from_range(timetable_ids)])
-        conn.execute(insert_supervision)
+        print_status("supervision", i)
+        try_insert(create_supervision_query, timetable_ids, user_ids)
+
+
+def create_supervision_query(timetable_ids, user_ids):
+    insert_supervision = supervision.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
+                                                     timetable_id=timetable_ids[random_from_range(timetable_ids)][0])
+    return insert_supervision
 
 
 def generate_grades():
     query = 'SELECT p.paper_id, dr.user_id FROM domain_reviewer dr JOIN paper p on p.domain_id = dr.domain_id'
     paper_user_ids = conn.execute(query).fetchall()
     for i in range(200):
-        paper_user = paper_user_ids[random_from_range(paper_user_ids)]
-        insert_grade = grade.insert().values(paper_id=paper_user["paper_id"],
-                                             user_id=paper_user["user_id"])
-        conn.execute(insert_grade)
+        print_status("grade", i)
+        try_insert(create_grade_query, paper_user_ids)
+
+
+def create_grade_query(paper_user_ids):
+    paper_user = paper_user_ids[random_from_range(paper_user_ids)]
+    insert_grade = grade.insert().values(paper_id=paper_user["paper_id"],
+                                         user_id=paper_user["user_id"])
+    return insert_grade
 
 
 def generate_domain_reviewers():
@@ -322,9 +388,14 @@ def generate_domain_reviewers():
     domain_ids = conn.execute(query1).fetchall()
     user_ids = conn.execute(query2).fetchall()
     for i in range(230):
-        insert_domain_reviewers = supervision.insert().values(user_id=user_ids[random_from_range(user_ids)],
-                                                              domain_id=domain_ids[random_from_range(domain_ids)])
-        conn.execute(insert_domain_reviewers)
+        print_status("domain_reviewer", i)
+        try_insert(create_domain_reviewers_query, domain_ids, user_ids)
+
+
+def create_domain_reviewers_query(domain_ids, user_ids):
+    insert_domain_reviewers = supervision.insert().values(user_id=user_ids[random_from_range(user_ids)][0],
+                                                          domain_id=domain_ids[random_from_range(domain_ids)][0])
+    return insert_domain_reviewers
 
 
 def generate_lectures():
@@ -338,13 +409,18 @@ def generate_lectures():
     paper_ids = conn.execute(query4).fetchall()
 
     for i in range(250):
-        insert_lecture = lecture.insert().values(user_id=user_ids[random_from_range(user_ids)],
-                                                 classroom_id=classroom_ids[random_from_range(classroom_ids)],
-                                                 timetable_id=timetable_ids[random_from_range(timetable_ids)],
-                                                 paper_id=paper_ids[random_from_range(paper_ids)],
-                                                 start_time=fake.time(),
-                                                 duration=random.randrange(45, 90))
-        conn.execute(insert_lecture)
+        print_status("lecture", i)
+        try_insert(create_lecture_query, classroom_ids, paper_ids, timetable_ids, user_ids)
+
+
+def create_lecture_query(classroom_ids, paper_ids, timetable_ids, user_ids):
+    insert_lecture = lecture.insert().values(user_id=user_ids[random_from_range(user_ids)],
+                                             classroom_id=classroom_ids[random_from_range(classroom_ids)],
+                                             timetable_id=timetable_ids[random_from_range(timetable_ids)],
+                                             paper_id=paper_ids[random_from_range(paper_ids)],
+                                             start_time=fake.time(),
+                                             duration=random.randrange(45, 90))
+    return insert_lecture
 
 
 def generate_participations():
@@ -354,35 +430,63 @@ def generate_participations():
     query2 = 'SELECT lecture_id FROM lecture'
     lecture_ids = conn.execute(query2).fetchall()
 
+    for i in range(len(participant_ids)):
+        print_status("participations", i)
+        try_insert(create_participants_query, lecture_ids, participant_ids)
+
+
+def create_participants_query(lecture_ids, participant_ids):
     insert_participants = participation.insert().values(user_id=participant_ids[random_from_range(participant_ids)],
                                                         lecture_id=lecture_ids[random_from_range(lecture_ids)])
-    conn.execute(insert_participants)
+    return insert_participants
 
 
 def random_from_range(collection):
     return randrange(len(collection))
 
 
+def try_insert(create_query, *args):
+    inserted = False
+    query = create_query(*args)
+    while not inserted:
+        try:
+            conn.execute(query)
+            inserted = True
+        except Exception:
+            query = create_query(*args)
+
+
+def try_insert_without_parameters(create_query):
+    inserted = False
+    query = create_query()
+    while not inserted:
+        try:
+            conn.execute(query)
+            inserted = True
+        except Exception:
+            query = create_query()
+
+
 # No foreign keys needed #
-generate_universities()
-generate_events()
-generate_tickets()
-generate_welcomepacks()
-generate_classrooms()
-generate_domains()
+# generate_universities()
+# generate_events()
+# generate_tickets()
+# generate_welcomepacks()
+# generate_classrooms()
+# generate_domains()
 
 # User and timetable #
-generate_users()
-generate_timetables()
+# generate_users()
+# generate_timetables()
 
 # Paper, administrator, reviewer, participant #
-generate_participants()
-generate_papers()
-generate_administrators()
-generate_reviewers()
+# generate_participants()
+# generate_papers()
+# generate_administrators()
+# generate_reviewers()
 
 # supervision, grade, domain_reviewer, lecture #
-generate_suprevisions()
+# generate_suprevisions()
 generate_grades()
 generate_domain_reviewers()
 generate_lectures()
