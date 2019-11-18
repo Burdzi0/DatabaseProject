5192eb41522fe8fd511adbb2ea15571d7910fd46
Burdzi0 <burdzi0@programmer.net>
2019-11-18 20:32:18 +0100

    Add prescripts

---
+++ docker_public_event.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.event (event_id, event_name, organizing_body, address, postal_code, town) VALUES (1, 'Richard Hines', 'White and Sons', '986 Cheryl Road Suite 513
+Marshallborough, CT 57759', '96214', 'Lake Joan');
+INSERT INTO public.event (event_id, event_name, organizing_body, address, postal_code, town) VALUES (2, 'Jennifer Jenkins', 'Barry-Garza', '1891 Joseph Causeway
+Jerryfort, IN 05595', '28951', 'Codychester');
+INSERT INTO public.event (event_id, event_name, organizing_body, address, postal_code, town) VALUES (3, 'Christopher Wheeler', 'Williams-Crawford', 'PSC 9271, Box 5044
+APO AP 41650', '64519', 'Lake Cynthiaberg');
+INSERT INTO public.event (event_id, event_name, organizing_body, address, postal_code, town) VALUES (4, 'Tonya Hernandez', 'Arnold-Davis', '685 Jennifer Canyon Apt. 755
+New Amy, GA 33438', '17017', 'Danaborough');
+INSERT INTO public.event (event_id, event_name, organizing_body, address, postal_code, town) VALUES (5, 'Patricia Jones', 'Brown, Harris and Rivera', '4686 Kerri Well Suite 357
+South Yolanda, OK 45046', '11367', 'Patriciatown');
---
+++ docker_public_supervision.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (1044, 2);
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (2279, 2);
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (578, 2);
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (1294, 2);
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (1678, 6);
+INSERT INTO public.supervision (user_id, timetable_id) VALUES (289, 3);
